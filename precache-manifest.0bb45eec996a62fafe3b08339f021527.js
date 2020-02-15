8b274e1b08c9fff06c3df21626643b360ca7941f
bilalarifsiddiqui <bilal.arif.siddiqui@gmail.com>
2020-02-15 22:14:24 +0500

    Updates

---
+++ precache-manifest.0bb45eec996a62fafe3b08339f021527.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b77105b2965f2fb846e5fd677e49a4ea",
+    "url": "/face-detection/index.html"
+  },
+  {
+    "revision": "bd450cea2872e99d931f",
+    "url": "/face-detection/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "699bf802a67a1854a8ad",
+    "url": "/face-detection/static/css/main.a6297a3b.chunk.css"
+  },
+  {
+    "revision": "bd450cea2872e99d931f",
+    "url": "/face-detection/static/js/2.52b2588f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/face-detection/static/js/2.52b2588f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "699bf802a67a1854a8ad",
+    "url": "/face-detection/static/js/main.8b8c8178.chunk.js"
+  },
+  {
+    "revision": "7fe7b15fc4560b38909a",
+    "url": "/face-detection/static/js/runtime-main.1e9f3881.js"
+  }
+]);
